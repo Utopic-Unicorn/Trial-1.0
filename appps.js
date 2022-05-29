284a3d017e8b10c57765a1f75632538b5385a4c1
Christian Barrios <99978665+Utopic-Unicorn@users.noreply.github.com>
2022-05-30 01:19:24 +0800

    for sure this is the last one

--- appps.js
+++ appps.js
@@ -8,18 +8,18 @@ const cap = document.querySelector('.caption')
 
 yesterday.addEventListener('click', function(){
     cardImage.src = 'https://images.unsplash.com/photo-1564238156626-c865046366cf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1331&q=80'
-    meme.innerText = 'Hey! That looks like me.'
+    meme.innerText = 'Lazy but cute Panda.'
     // console.log('clicked')
 })
 
 today.addEventListener('click', function(){
     cardImage.src = 'https://images.unsplash.com/photo-1564349683136-77e08dba1ef7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80'
-    meme.innerText = 'A cute Panda that looks like me.'
+    meme.innerText = 'No diet but still cute Panda.'
 })
 
 tomorrow.addEventListener('click', function(){
     cardImage.src = 'https://images.unsplash.com/photo-1562683687-80cb85c493b4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
-    meme.innerText = 'What I look like tomorrow.'
+    meme.innerText = "Starting to excercise Panda, hope he doesn't hurt himself."
 })
 
 // if (emege.src = 'https://images.unsplash.com/photo-1456926631375-92c8ce872def?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'){
